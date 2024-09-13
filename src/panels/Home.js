d39b63dc347d5ac8074bc3fb13aebf6d6d7623b4
Анна <368943@niuitmo.ru>
2024-09-14 01:04:10 +0300

    add first version of lab_0

---
+++ src/panels/Home.js
@@ -0,0 +1,63 @@
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
+import PropTypes from 'prop-types';
+import bridge from '@vkontakte/vk-bridge';
+
+export const Home = ({ id, fetchedUser }) => {
+  const { photo_200, city, first_name, last_name } = { ...fetchedUser };
+  // const routeNavigator = useRouteNavigator();
+
+  async function openStoryEditor() {
+    const imageUrl = (await (await fetch("https://dog.ceo/api/breeds/image/random")).json()).message;
+
+    bridge.send('VKWebAppShowStoryBox', {
+      background_type: 'image',
+      url : imageUrl,
+      })
+      .then((data) => {
+        if (data.code_data) {
+          // Редактор историй открыт
+          console.log(data);
+        }})
+      .catch((error) => {
+        // Ошибка
+        console.log(error);
+      });
+  }
+
+  return (
+    <Panel id={id}>
+      <PanelHeader>Главная</PanelHeader>
+      {fetchedUser && (
+        <Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+          <Cell before={photo_200 && <Avatar src={photo_200} />} subtitle={city?.title}>
+            {`${first_name} ${last_name}`}
+          </Cell>
+        </Group>
+      )}
+
+      <Group header={<Header mode="secondary">Navigation Example</Header>}>
+        <Div>
+          {/* <Button stretched size="l" mode="secondary" onClick={() => routeNavigator.push('persik')}>
+            Покажите Персика, пожалуйста!
+          </Button> */}
+          <Button stretched size="l" mode="secondary" onClick={openStoryEditor}>
+            Редактор историй
+          </Button>
+        </Div>
+      </Group>
+    </Panel>
+  );
+};
+
+Home.propTypes = {
+  id: PropTypes.string.isRequired,
+  fetchedUser: PropTypes.shape({
+    photo_200: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    city: PropTypes.shape({
+      title: PropTypes.string,
+    }),
+  }),
+};
